e7c2697208283b60fb18ac42c2dac3c051b5da6a
andy202003 <aloayza@uoc.edu>
2020-12-09 15:11:21 -0500

    Third commit

--- dist/slider.ab09d017.js
+++ dist/slider.ab09d017.js
@@ -361,7 +361,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45113" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33159" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
